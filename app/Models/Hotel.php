77aac8c1229948d75f77cbdaaab2c5dc1dfcdee9
sumitsehgal <theprofessional1992@gmail.com>
2023-08-21 22:39:17 +0530

    Hotel Revised Schema

--- app/Models/Hotel.php
+++ app/Models/Hotel.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Hotel extends Model
 {
-    use HasFactory;
+    use HasFactory, Sluggable;
+
+    public function sluggable(): Array
+    {
+        return [
+            'slug' => [
+                'source' => 'name'
+            ]
+        ];
+    }
 }
