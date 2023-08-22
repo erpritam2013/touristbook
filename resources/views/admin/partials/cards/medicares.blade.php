112621065606ab6587a3ec38af2d10e2b0e948d0
sumitsehgal <theprofessional1992@gmail.com>
2023-08-22 10:01:23 +0530

    Changes

--- resources/views/admin/partials/cards/medicares.blade.php
+++ resources/views/admin/partials/cards/medicares.blade.php
@@ -4,7 +4,7 @@
     </div>
     <div class="card-body">
         <div class="form-group row">
-            <p>{{$medicares}}</p>
+        @include('admin.partials.utils.nested_checkbox_list', ['items' => $medicares, 'name'=> 'medicares'])
         </div> 
     </div>
 </div>
--- resources/views/admin/partials/cards/places.blade.php
+++ resources/views/admin/partials/cards/places.blade.php
@@ -4,7 +4,7 @@
     </div>
     <div class="card-body">
         <div class="form-group row">
-            <p>{{$places}}</p>
+            @include('admin.partials.utils.nested_checkbox_list', ['items' => $places, 'name'=> 'places'])
         </div> 
     </div>
 </div>
