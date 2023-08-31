34f32d5a1fae101ca6db758666dfe40f543341bd
sumitsehgal <theprofessional1992@gmail.com>
2023-09-01 03:48:01 +0530

    Map Placed

--- public/admin-part/js/tourist-lib.js
+++ public/admin-part/js/tourist-lib.js
@@ -1,4 +1,14 @@
 $(document).ready(function () {
+    // Map Variable
+    const addressTextbox = document.getElementById('map_address');
+    const latitudeTextbox = document.getElementById('latitude');
+    const longitudeTextbox = document.getElementById('longitude');
+    const zoomTextbox = document.getElementById('zoom_level');
+
+    // Markers on Map : Assuming there will be one Map on a Page
+    var markers = [];
+
+
     var base_admin_url = $('#base-admin-url').val()
     let btnAddSubForm = $(".btn-add-subform")
     if (btnAddSubForm.length > 0) {
@@ -56,5 +66,97 @@ $(document).ready(function () {
 
         }) // On Click Event Block Ends
     } // If Block Ends
+
+    // Initialize Map
+    function initMap() {
+        map = new google.maps.Map(document.getElementById("map"), {
+            zoom: 1, // Default
+            center: new google.maps.LatLng(30.704649, 76.717873),
+            mapTypeId: "terrain",
+        });
+
+        console.log(google.maps.places)
+    }
+    // TODO: Call If I need to Load Map
+    initMap();
+
+    // Set markup on Map
+    const setMarkupOnMap = (geometryList) => {
+        if(map) {
+            if(markers.length > 0) {
+                // Clear all markers.
+                markers.forEach(function(marker) {
+                    marker.setMap(null);
+                });
+            }
+
+            geometryList.forEach((latLng) => {
+                // Add to Markers for Further access
+                markers.push(new google.maps.Marker({
+                    position: new google.maps.LatLng(latLng.latitude, latLng.longitude),
+                    map: map
+                }))
+            })
+
+            // Listen for the marker's position change event.
+            let lastAddedMarker = markers[markers.length - 1];
+            map.setCenter(lastAddedMarker.getPosition());
+        }
+    }
+
+    // Set Zoom Level
+    const setZoomLevel = (level) => {
+        map.setZoom(level);
+    }
+
+    // Place Changed
+    const onPlaceChanged = () => {
+        console.log("Place Changed Trigger")
+        let latitude = latitudeTextbox.value;
+        let longitude = longitudeTextbox.value;
+        let zoomLevel = parseInt(zoomTextbox.value);
+
+        let geometryList = [
+            {
+                latitude,
+                longitude
+            }
+        ]
+
+        setMarkupOnMap(geometryList)
+        setZoomLevel(zoomLevel)
+
+    }
+
+    // Autocompletion Addres Bar
+    const autocomplete = new google.maps.places.Autocomplete((addressTextbox),
+        { types: ['geocode'] });
+
+      // Event listener for the autocomplete object.
+      autocomplete.addListener('place_changed', function() {
+        // Get the selected place.
+        const place = autocomplete.getPlace();
+        // Set the address textbox to the selected place's address.
+        addressTextbox.value = place.formatted_address;
+
+        let latitude = place.geometry.location.lat();
+        let longitude = place.geometry.location.lng();
+
+        latitudeTextbox.value = latitude
+        longitudeTextbox.value = longitude
+
+        onPlaceChanged()
+    });
+
+    latitudeTextbox.onchange = onPlaceChanged
+    longitudeTextbox.onchange = onPlaceChanged
+    zoomTextbox.onchange = onPlaceChanged
+    zoomTextbox.onkeyup = onPlaceChanged
+
+
+
+
+
+
 
 })
--- resources/views/admin/layout-parts/foot.blade.php
+++ resources/views/admin/layout-parts/foot.blade.php
@@ -6,9 +6,24 @@
     <script src="{!! asset('admin-part/vendor/global/global.min.js') !!}"></script>
     <script src="{!! asset('admin-part/js/quixnav-init.js') !!}"></script>
     <script src="{!! asset('admin-part/js/custom.min.js') !!}"></script>
+
+    {{-- TODO: Google Map Library; Recommended by Google --}}
+    {{-- <script>
+        (g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({
+          key: "AIzaSyCF8MnYK1Ft-lPa3_B6rirg2IJzptB4m1Y",
+          v: "weekly",
+
+        });
+    </script> --}}
+
+    <script
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCF8MnYK1Ft-lPa3_B6rirg2IJzptB4m1Y&v=weekly&libraries=places"
+        defer
+    ></script>
+
     <script src="{!! asset('admin-part/js/touristbook-terms-custom.js') !!}"></script>
     <script src="{!! asset('admin-part/js/tourist-lib.js') !!}"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/js/all.min.js"></script>
-    
+
 
     @show
