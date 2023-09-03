d79817475ca37cb1d10cdfe75182eec12c58b640
erpritam2013 <erpritam2013@gmail.com>
2023-09-03 13:15:31 +0530

    new column update in location

--- resources/views/admin/partials/utils/subform.blade.php
+++ resources/views/admin/partials/utils/subform.blade.php
@@ -36,37 +36,8 @@
            
             @endif
         @endforeach
-    <li class="subform-card">
-        <div class="card">
-            <div class="card-header border-bottom">
-                <h4 class="card-title">
-                    <span class="ui-icon ui-icon-arrowthick-2-n-s"></span>
-                    <span class="card-title-text">&nbsp;</span>
-                </h4>
-                <div class="float-left">
-                    <a href="javascript:void(0);" class="edit-card"><i class="fa fa-edit"></i></a>
-                    <a href="javascript:void(0);" class="delete-card"><i class="fa fa-times-circle"></i></a>
-                </div>
-            </div>
-
-            <div class="card-body">
-            @foreach($card as $controlId => $value)
-                @if(isset($typeFields[$controlId]))
-                <div class="form-group row">
-                    <label class="col-lg-3 col-form-label">{{ $typeFields[$controlId]['label'] }}</label>
-                    <div class="col-lg-9">
-                        @if($typeFields[$controlId]['control'] == "text")
-                            <input type="text" class="form-control" name="{{$type}}[{{$key}}][{{$controlId}}]" value="{{$value ?? ''}}" >
-                        @elseif($typeFields[$controlId]['control'] == "textarea")
-                            <textarea class="form-control" name="{{$type}}[{{$key}}][{{$controlId}}]" >{{$value ?? ''}}</textarea>
-                        @endif
-                    </div>
-                </div>
-                @endif
-            @endforeach
-            </div>
         </div>
-    </li>
+    </div>
 
 
     @endforeach
