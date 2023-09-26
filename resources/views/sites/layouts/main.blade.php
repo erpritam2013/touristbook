c1c6b97fa9dc19be9f299d18c8c6338c66fff2de
sumitsehgal <theprofessional1992@gmail.com>
2023-09-27 01:37:06 +0530

    Layout Added for Front Page

---
+++ resources/views/sites/layouts/main.blade.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html lang="en">
+
+<!-- Mirrored from html.themefax.com/travelgo/ by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 23 Sep 2023 14:50:53 GMT -->
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" id="google-font-css-css" href="https://fonts.googleapis.com/css?family=Poppins:400,500,600&#038;ver=6.3" type="text/css" media="all" />
+    <!-- Bootstrap CSS -->
+    <link href="{{ asset('sites/css/style.css')}}" type="text/css" rel="stylesheet" />
+    <link href="{{ asset('sites/css/bootstrap-datepicker.css')}}" type="text/css" rel="stylesheet" />
+    <!-- Favicon -->
+    <!-- Favicon and Touch Icons -->
+    <link href="{{ asset('sites/images/favicon.png') }}" rel="shortcut icon" type="image/png">
+    <title>Tourist Book</title>
+
+    <style>
+        a.btn-action {
+            background: #222;
+            color: #fff;
+            padding: 9px 13px;
+            margin: 0 0 0 15px;
+        }
+    </style>
+</head>
+
+<body>
+
+    @include('sites.partials.header')
+
+    @yield('content')
+
+    @include('sites.partials.newsletter')
+
+
+    @include('sites.partials.footer')
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="{{asset('sites/js/jquery.min.js')}}" type="text/javascript"></script>
+    <script src="{{asset('sites/js/popper.min.js')}}" type="text/javascript"></script>
+    <script src="{{asset('sites/js/bootstrap.min.js')}}" type="text/javascript"></script>
+    <script src="{{asset('sites/js/functions.js')}}" type="text/javascript"></script>
+    <script src="{{asset('sites/js/owl.carousel.min.js')}}" type="text/javascript"></script>
+    <script src="{{asset('sites/js/slick.js')}}" type="text/javascript"></script>
+    <script src="{{asset('sites/js/swiper.min.js')}}" type="text/javascript"></script>
+    <script src="{{asset('sites/js/main.js')}}" type="text/javascript"></script>
+    <script src="{{asset('sites/js/jquery.fancybox.min.js')}}" type="text/javascript"></script>
+    <script src="{{asset('sites/js/bootstrap-datepicker.min.js')}}" type="text/javascript"></script>
+    <script src="{{asset('sites/js/jquery-ui.min.js')}}" type="text/javascript"></script>
+</body>
+
+
+</html>
