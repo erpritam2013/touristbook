248d8befdaf0ad4c6267e5d1620771f4f30ca54d
sumitsehgal <theprofessional1992@gmail.com>
2023-10-01 00:27:14 +0530

    Hotel Listing

---
+++ resources/views/sites/partials/filters/propertyTypeFilter.blade.php
@@ -0,0 +1,24 @@
+<div class="mb-left">
+    <label for="form_property_types" class="form-label">Property Type</label>
+    <div class="form-group">
+        @if(!empty($filterPropertyTypes))
+        <ul class="list-unstyled mb-0">
+            @foreach($filterPropertyTypes as $key=>$propertyType)
+            <li class="{{ $key > 2 ? 'li-hide' : '' }}">
+                <div class="custom-control custom-checkbox">
+                    <input type="checkbox" id="property_{{$key}}" name="propertyTypes[]"
+                        class="custom-control-input filter-option filter-property-types" value="{{$propertyType['id']}}">
+                    <label for="property_{{$key}}" class="custom-control-label">{{$propertyType['name']}}</label>
+                </div>
+            </li>
+            @endforeach
+        </ul>
+        @if($filterPropertyTypes->count() > 3)
+        <a href="javascript:void(0)" class="more-li">More <i class="fa fa-caret-down"></i></a>
+        @endif
+        @endif
+
+
+    </div>
+
+</div>
