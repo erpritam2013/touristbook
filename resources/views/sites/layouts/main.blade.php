d37cac509b74345762994d4fec183e318b4e6654
erpritam2013 <erpritam2013@gmail.com>
2023-10-24 11:10:11 +0530

    tour frontend style work

--- resources/views/sites/layouts/main.blade.php
+++ resources/views/sites/layouts/main.blade.php
@@ -27,7 +27,7 @@
     </style>
 </head>
 
-<body class="{{touristbook_sanitize_title($body_class) ?? ''}}">
+<body class="{{touristbook_sanitize_title($body_class ?? '') ?? ''}}">
 
     <input type="hidden" id="base-url" value="{{route('home')}}" />
 
