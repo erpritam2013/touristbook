203c31a27975c8a30e7ce3249116f944e4553bce
erpritam2013 <erpritam2013@gmail.com>
2023-11-20 10:54:33 +0530

    destination detail page some issue fixed

--- resources/views/sites/partials/location-details/shopaholics-anonymous.blade.php
+++ resources/views/sites/partials/location-details/shopaholics-anonymous.blade.php
@@ -14,9 +14,12 @@
 				<div class="item">
 					<div class="thumbnail shopaholics-anonymous-image">
 						@php 
-						$shopaholics_anonymous_image = (!empty($shopaholics_anonymous['shopaholics_anonymous-image']) && isset($shopaholics_anonymous['shopaholics_anonymous-image'][0]['id']))?getConversionUrl($shopaholics_anonymous['shopaholics_anonymous-image'][0]['id'],'270x200'):null;
+						if(!empty($shopaholics_anonymous['shopaholics_anonymous-image'])){
+						$shopaholics_anonymous_image_arr = json_decode($shopaholics_anonymous['shopaholics_anonymous-image'],true);
+						$shopaholics_anonymous_image = (!empty($shopaholics_anonymous_image_arr) && isset($shopaholics_anonymous_image_arr[0]['id']))?getConversionUrl($shopaholics_anonymous_image_arr[0]['id'],'270x200'):null;
+						}
 						@endphp
-						<img class="" src="{{$shopaholics_anonymous_image ?? asset('sites/images/dummy/270x200.jpg')}}" alt="{{strtolower($shopaholics_anonymous['shopaholics_anonymous-title'])}}-image">
+						<img class="" src="{{$shopaholics_anonymous_image ?? asset('sites/images/dummy/270x200.jpg')}}" alt="{{strtolower($shopaholics_anonymous['shopaholics_anonymous-title'])}} image">
 					</div>
 				</div>
 			</div>
